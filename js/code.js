65090e7d7dbdd6cc6ac7bc0a1d47de55ab951fa9
Breno Gullo <brebueno@hotmail.com>
2021-11-29 09:49:26 -0300

    Paginas adicionais

--- js/code.js
+++ js/code.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+// Função Encapsulada: Preenche o SELECT com os dados da API do IBGE
+(Function(){
+
+    var url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+    var select = document.getElementById('estado');
+    var options = '<option>Selecione</option>';
+
+    // Consulta a API com o método fetch e com um laço de repetição incrementa as tags option
+    fetch(url).then(Response => response.json()).then(json => console.log(json)).catch();
+
+    select.innerHTML= options;
+
+})();
